48afd637018f5eb0a99d49043d3d7141e3295426
Riccardo Colella <colella.git@outlook.com>
2020-10-23 18:08:01 +0200

    [consumer.scala] Professional commit n1

--- consumer/Consumer.scala
+++ consumer/Consumer.scala
@@ -1,31 +1,54 @@
-import com.datastax.driver.core.Cluster
-import com.datastax.spark.connector.SomeColumns
-import org.apache.commons.codec.StringDecoder
+import java.util.HashMap
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka._
+import kafka.serializer.{DefaultDecoder, StringDecoder}
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import io.confluent.kafka.serializers.KafkaAvroDecoder
-import org.apache.spark._
+import org.apache.spark.SparkContext
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
-import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.storage.StorageLevel
+import java.util.{Date, Properties}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
+import scala.util.Random
+import com.datastax.spark.connector._
+import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
+import com.datastax.spark.connector.streaming._
+import scala.util.parsing.json._
+import scala.collection.mutable
 
 object Consumer {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("CreditCardsFraudDetector")
-    // Streaming data will be divided into batches every 1 second
-    val streamingContext = new StreamingContext(conf, Seconds(1))
 
-    streamingContext.checkpoint("file:///tmp/spark/checkpointForCreditCardsFraudDetector")
+    val conf = new SparkConf().setAppName("csv").setMaster("local[2]")
+    val sc = SparkContext.getOrCreate(conf)
+    val ssc = new StreamingContext(sc, Seconds(1))
+
+    ssc.checkpoint("./checkpoint")
+
+    val kafkaConf = Map(
+      "metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
+      "group.id" -> "kafka-spark-streaming",
+      "zookeeper.connection.timeout.ms" -> "1000")
+
+    val topicSet = Set("credit-transactions")
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, topicSet)
+    
+    val values = messages.map(message => message._2.split(",")))
+    values.print()
 
 
+    ssc.start()
+    ssc.awaitTermination()
+    /*
     // Cassandra initialization
-    //val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
-    //val session = cluster.connect()
+    val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+    val session = cluster.connect()
 
-    //session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 1};")
-    //session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
-    //session.close()
+    session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 1};")
+    session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
+    session.close()
 
 
     val kafkaConf = Map(
@@ -34,26 +57,22 @@ object Consumer {
       "group.id" -> "kafka-spark-streaming",
       "zookeeper.connection.timeout.ms" -> "1000")
 
-    val topicsSet = Set("transactions")
-    val messages = KafkaUtils.createDirectStream[Object, Object, KafkaAvroDecoder, KafkaAvroDecoder](streamingContext, kafkaConf, topicsSet)
-    
+    val topicSet = Set("transactions")
+    //val messages = KafkaUtils.createDirectStream[Object, Object, KafkaAvroDecoder, KafkaAvroDecoder](streamingContext, kafkaConf, topicsSet)
 
-    //val messages= KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-  //streamingContext,
-  //PreferConsistent,
-  //Subscribe[String, String](kafkaParams,topicsSet)
-//)
-    
-    //println(messages)	
+
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](streamingContext, kafkaConf, topicsSet)
+
+    println(messages)
 
     // split values of the messages upon the comma
     val values = messages.map(message => message._2)
     println(values)
-    val pairs = values.map(row => return (row(0), row(1)))
+    //val pairs = values.map(row => return (row(0), row(1)))
 
     // Here I'm wasting all useless information for the visualization purpose
     //val usefulData = pairs.map(pair => cleanTransaction(pair))
-    
+
     //println(usefulData)
 
     //val stateDStream = usefulData
@@ -63,11 +82,15 @@ object Consumer {
     streamingContext.start()
     streamingContext.awaitTermination()
   }
-  
+
   def cleanTransaction(transaction: Array[String]): Array[Int] = {
-      // keep only: 0-> transaction time; 29->transaction amount; 30->class (fraudulent or not)
-      Array(transaction(0).toInt, transaction(29).toInt, transaction(30).toInt)
-    }
+    // keep only: 0-> transaction time; 29->transaction amount; 30->class (fraudulent or not)
+    Array(transaction(0).toInt, transaction(29).toInt, transaction(30).toInt)
+
+ */
+  }
 
 }
+
+
 
